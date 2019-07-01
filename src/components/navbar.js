e5d52f60a613396cc8a4419c74bf3c97d1a6037a
Tsvetelina Koleva <tsekoleva@gmail.com>
2019-07-01 23:46:41 +0100

    adding partners page + content

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => (
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/sponsors">Sponsors</Link></li>
+        <li><Link to="/partners">Partners</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
